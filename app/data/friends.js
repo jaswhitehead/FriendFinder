f7d126338580c63dda57b2d09548a83f0ce2815d
Jason Whitehead <jaswhitehead@gmail.com>
2020-07-17 15:58:03 -0500

    Rearranged API routes file and code for Survey.html. Added smaller pictures to fit in modals

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,47 +3,47 @@ var friendsArray = [
 
     {
         "name": "Beth Humphrey",
-        "photo": "https://media.licdn.com/https://scontent.fbna1-1.fna.fbcdn.net/v/t1.0-9/98206846_2968652166521938_7305901947893055488_n.jpg?_nc_cat=110&_nc_sid=09cbfe&_nc_ohc=3zZz0dr3lxwAX-zWAxT&_nc_ht=scontent.fbna1-1.fna&oh=29cffc246078a8889cddf293965e56c2&oe=5F29A5CE/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSaORwXKcqwWv3HkkiXzBev8rSVTHtsx3lqbA&usqp=CAU",
         "scores": ["5","3","1","2","4","1","5","3","2","2"]
     },
     {
         "name": "Margot Robbie",
-        "photo": "https://pbs.twimg.com/https://cdn.telanganatoday.com/wp-content/uploads/2019/01/Margot-Robbie.jpg/691785039043022849/oWsy8LNR.jpg",
+        "photo": "https://i.ebayimg.com/images/g/2k8AAOSw9NdXtpTR/s-l300.jpg",
         "scores": ["4","2","5","1","3","2","2","1","3","2"]
     },
     {
     "name": "Tom Hardy",
-    "photo": "https://avatars2.https://www.biography.com/.image/t_share/MTE5NTU2MzE2NjUyOTk2MTA3/9th-annual-ves-awards---red-carpet.jpg.com/u/8504998?v=3&s=460",
+    "photo": "https://i.pinimg.com/564x/a8/e9/87/a8e987ee86540f2730bc77c91cac1b20--tommy-boy-tom-hardy.jpg",
     "scores": ["5","2","2","2","4","1","3","2","5","5"]
     },
     {
         "name": "Scarlett Johansson",
-        "photo": "https://pbs.twimg.com/https://i.insider.com/599f1c4a24fbd124008b46fe?width=1100&format=jpeg&auto=webp/639214960049000449/lNCRC-ub.jpg",
+        "photo": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F38%2F2014%2F03%2F12233704%2FScarlet-Johansson-and-Romain-Dauriac-are-Expecting-Their-First-Child-200x300.jpg",
         "scores": ["3","3","4","2","2","1","3","2","2","3"]
     },
     {
         "name": "Adam Levine",
-        "photo": "https://media.licdn.com/https://static01.nyt.com/images/2018/07/08/fashion/05LIST-INYT2/05LIST-INYT2-superJumbo.jpg/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "photo": "https://cdn.hswstatic.com/gif/adam-levine-yoga-468x622-1.jpg",
         "scores": ["4","3","4","1","5","2","5","3","1","4"]
     },
     {
         "name": "John Bell",
-        "photo": "https://media.licdn.com/mpr/mpr/https://3.bp.blogspot.com/-AIY8vZEsM14/TVcN0OZhdtI/AAAAAAAABBs/XcdQKDvNZMw/s1600/John%2BBell.jpg/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "photo": "https://i.pinimg.com/236x/67/f5/d6/67f5d629def0ed1298403e40e2a3c437--widespread-panic-live-rock.jpg",
         "scores": ["4","4","2","3","2","2","3","2","4","5"]
     },
     {
         "name": "Jason Whitehead",
-        "photo": "https://scontent.fbna1-2.fna.fbcdn.net/v/t1.0-9/39296_4730091015939_1452621106_n.jpg?_nc_cat=101&_nc_sid=cdbe9c&_nc_ohc=FEozHF3RIbkAX8N0EeH&_nc_ht=scontent.fbna1-2.fna&oh=89302d4f376f030720c4a9ba47846046&oe=5F294C27",
-        "scores": ["5","3","1","2","4","1","5","3","2","2"]
+        "photo": "/https://jaswhitehead.github.io/Bootstrap-Portfolio/CSS/Images//highres_144358222.jpeg",
+        "scores": ["5","3","1","1","5","1","5","3","2","2"]
     }, 
     {
         "name": "Kate Beckinsale",
-        "photo": "https://www.maxim.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY5MTE4MTA3OTkyODU5OTM3/kate-beckinsale-promo.jpg",
-        "scores": ["1","3","5","2","4","3","5","3","1","2"]
+        "photo": "https://cleo.com.my/wp-content/uploads/2017/01/shutterstock_541798573_Easy-Resize.com_-200x300.jpg",
+        "scores": ["1","3","3","3","4","3","5","3","1","2"]
     },
     {
         "name": "Mila Kunis",
-        "photo": "https://www.caa.com/sites/default/files/styles/headshot_500x500/public/2019-07/Kunis_CAASpeakers_Headshot.jpg?itok=ho3100AP",
+        "photo": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F38%2F2014%2F05%2F12231923%2FMila-Kunis-is-Planning-a-Natural-Birth-200x300.jpg",
         "scores": ["1","5","2","5","1","3","3","2","5","2"]
     }  
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,22 +3,69 @@ var friendData = require("../data/friends");
 //Routes
 module.exports = function (app, data) {
 
-    // API GET Requests
+    // API GET Request
     app.get("/api/friends", function (req, res) {
-        return res.json(friendData);
+        res.json(friendData);
     });
 
-    // API POST Requests
+    //API Post Request
     app.post("/api/friends", function (req, res) {
         var newFriend = req.body;
-        for (var i = 0; i < newFriend.scores.length; i++) {
-            var string = newFriend.scores[i];
-            var int = parseInt(string);
-            newFriend.scores.splice(i, 1, int);
-        }
 
-        data.push(newFriend);
-        res.json(newFriend);
+        friendData.push(newFriend);
+
+        //Global variable
+        var compatibilityGoal = 0;
+        var compatibilityArray = [];
+
+        // Sum of all values in an array
+        const arrSum = arr => arr.reduce((a, b) => a + b, 0);
+
+        // This function will check the new users compatibility based on question results
+        function userCompatibility() {
+            for (var i = 0; i < friendData.length - 1; i++) {
+                arrIndex = i;
+                console.log("Compability Function is currently being run on " + friendData[arrIndex].name);
+                differences(arrIndex);
+            }
+            // This block of code checks the total difference array for the person who has the closest diff to zero
+            closest = compatibilityArray.reduce(function (prev, curr) {
+                return (Math.abs(curr - compatibilityGoal) < Math.abs(prev - compatibilityGoal) ? curr : prev);
+            });
+
+            console.log("The most compatible score is: " + closest);
+            mostCompatible();
+        };
+        // Helper function that checks differences between user score and scores inside data array
+        function differences(index) {
+            let diffAnswer = [];
+            for (var i = 0; i < newFriend.scores.length; i++) {
+                let diffBoth = Math.abs(newFriend.scores[i] - friendData[arrIndex].scores[i]);
+                diffAnswer.push(diffBoth);
+            }
+            totalDifference = arrSum(diffAnswer);
+            console.log("Total Difference between " + newFriend.name + " & " + friendData[arrIndex].name + " is: " + totalDifference);
+            compatibilityArray.push(totalDifference);
+        };
+        // Function to find the most compatible user
+        function mostCompatible() {
+            for (var i = 0; i < compatibilityArray.length; i++) {
+                if (closest === compatibilityArray[i]) {
+                    // Here the results were logged to get them on node terminal
+                    console.log("The most compatible score has an index of: " + i);
+                    console.log("The most compatible is: " + friendData[i].name);
+                    console.log("The most compatible has a picture of: " + friendData[i].photo);
+                    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    // Here the result is placed in a variable that is then sent back the client
+                    var matched = friendData[i];
+                    console.log(matched);
+                    return res.send(matched);
+                }
+            }
+        };
+
+        // Function calls
+        userCompatibility();
+
     });
-}
-
+};
